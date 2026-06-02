7dfb0df0ba2ef88306197193c91caf06052e9fb7
santi1403 <santibautista720@gmail.com>
2026-06-01 21:22:34 -0500

    Update logica.php

--- controlador/logica.php
+++ controlador/logica.php
@@ -2,12 +2,12 @@
 
 var_dump($_POST);
 
-$conexion = new PDO('pgsql:host=servidor;dbname=nombre_BD','user','password');
-$registrar = $conexion->prepare("INSERT INTO form_sugerencias (nombre,telefono,detalles) VALUES (?, ?, ?)");
-$registrar->execute([$_POST["nom"], $_POST["tel"], $_POST["det"]]);
+$conexion = new PDO('pgsql:host=@dpg-d8f39bl53gjs739kr1c0-a.oregon-postgres.render.com;dbname=sena_4gjt_user','sena_4gjt','MdEyvmMNdVywTNkoijpetMBysaRHzQxD');
+$registrar = $conexion->prepare("INSERT INTO aprendices (nombre,telefono,detalles) VALUES (?, ?)");
+$registrar->execute([$_POST["nom"], $_POST["tel"] ]);
 echo "<p style='color:white;background-color:green;font-family:calibri,arial;font-size:24px;text-align:center'>Registro exitoso</p>";
 
-$consulta = $conexion->prepare("SELECT * FROM form_sugerencias order by id");
+$consulta = $conexion->prepare("SELECT * FROM aprendices order by id");
 $consulta->execute();
 $tabla = $consulta->fetchAll(PDO::FETCH_ASSOC);	      //PDO::FETCH_NUM
 $conexion = null;
@@ -15,12 +15,12 @@ $conexion = null;
 echo "<table><tr><th>Codigo</th>
                  <th>Nombre completo</th>
                  <th>Contacto</th>
-			     <th>Detalles</th>		</tr>";
+			     	</tr>";
 foreach($tabla as $fila){		//Recorre el arreglo $tabla como FETCH_NUM
     echo "<tr>		<td>$fila[id]</td>
             		<td>$fila[nombre]</td>
             		<td>$fila[telefono]</td>
-            		<td>$fila[detalles]</td>		</tr>";
+            			</tr>";
 }
 echo "</table>";
 
